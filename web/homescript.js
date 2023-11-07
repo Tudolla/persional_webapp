5044d5098158a969ea837fa3719f99c85f3afa1a
Tudolla <nguyenvantu.cnpm@gmail.com>
2023-11-07 18:15:25 +0700

    error in cart

--- web/homescript.js
+++ web/homescript.js
@@ -19,20 +19,23 @@ btn.forEach(function(button, index){
        
        addCart(productImg,productName,productPrice);
        
+       
    });
 });
 
 function addCart(productImg,productName,productPrice){
     var addtr = document.createElement("tr");
-    var trcontent = '<tr><td style="display:flex; align-items: center"><img style="width:70px;" src="'+productImg+'" alt="">'+productName+'</td><td><span>'+productPrice+'</span><sup>VND</sup></td><td><input style="width: 30px; outline: none" type="number" value="1" min="0"></td><td style="cursor:pointer">Xoa</td></tr>';
+    var cartItem = document.querySelectorAll("tbody tr");
+    for(var i = 0; i<cartItem.length; i++){
+        var productT = document.querySelectorAll(".title");
+        if(productT[i].innerHTML == productName){
+            alert("Your product had been in cart");
+            return
+        }
+    }
+    var trcontent = '<tr><td style="display:flex; align-items: center"><img style="width:70px;" src="'+productImg+'" alt=""><span class="title">'+productName+'</span></td><td><p><span class="price">'+productPrice+'</span><sup>VND</sup></p></td><td><input style="width: 30px; outline: none" type="number" value="1" min="1"></td><td style="cursor:pointer"><span class="cartDelete">Delete</span></td></tr>';
     
-                        
-                            
-                            
-                            
-                            
-
-                        
+                     
                         
     addtr.innerHTML = trcontent;
     
@@ -40,6 +43,8 @@ function addCart(productImg,productName,productPrice){
     
     cartTable.append(addtr);
     cartTotal();
+    deleteCart();
+   
     
 }
 // ----------tinh total Price
@@ -48,18 +53,57 @@ function cartTotal(){
     var totalRes = 0;
     for(var i=0;i<cartItem.length;i++){
         var inputValue = cartItem[i].querySelector("input").value;
-        var productPrice = cartItem[i].querySelector("span").innerText;
+        var productPrice = cartItem[i].querySelector(".price").innerText;
         
         var   total = inputValue * productPrice*1000;
         
         
-        totalRes +=total;
-        console.log(totalRes);
+        totalRes = totalRes + total;
+        inputChange();
+//        console.log(totalRes);
         
     }
     
     var totalResult = document.querySelector(".price-total span");
+    var cartPrice = document.querySelector(".cart-icon span");
     
-    totalResult.innerHTML = totalRes;
+    totalResult.innerHTML = totalRes.toLocaleString('de-DE');
+    cartPrice.innerHTML = totalResult.toLocaleString('de-DE');
     
 }
+
+
+function deleteCart(){
+    var cartItem = document.querySelectorAll("tbody tr");
+    for(var i = 0; i< cartItem.length; i++){
+        var productT = document.querySelectorAll(".cartDelete");
+        productT[i].addEventListener("click",function(event){
+            var cartDelete = event.target;
+            var cartItemR = cartDelete.parentElement.parentElement;
+            cartItemR.remove();
+            cartTotal();
+            
+        });
+        
+    }
+}
+
+function inputChange(){
+    var cartItem = document.querySelectorAll("tbody tr");
+    for(var i = 0; i<cartItem.length; i++){
+        var inputValue = cartItem[i].querySelector("input");
+        inputValue.addEventListener("change", function(){
+            cartTotal();
+        });
+    }
+}
+
+const cartBtn = document.querySelector(".fa-times");
+const cartShow = document.querySelector(".fa-shopping-cart");
+cartShow.addEventListener("click", function(){
+    document.querySelector(".cart").style.right = "0"
+})
+
+cartBtn.addEventListener("click",function(){
+    document.querySelector(".cart".style.right="-100%");
+});
